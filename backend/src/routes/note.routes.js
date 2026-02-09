4e0693a781f3dbced9a9a5aad8029a8f2e8b159a
Areeba Hanif <areeba.hanif222@gmail.com>
2026-02-09 23:29:18 +0500

    Issues solved in middleware

--- backend/src/routes/note.routes.js
+++ backend/src/routes/note.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { create, getAll, update, remove } = require("../controllers/note.controller");
+
 const { protect } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
